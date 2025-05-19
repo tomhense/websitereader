a596a0b9e5adaa7fed66959ad988e586fc8b6b45
Tom <>
2025-05-19 12:22:30 +0200

    Fixed media playback in background

--- app/src/main/java/com/example/websitereader/PlaybackService.kt
+++ app/src/main/java/com/example/websitereader/PlaybackService.kt
@@ -5,23 +5,25 @@ import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
 class PlaybackService : MediaSessionService() {
+
     private var mediaSession: MediaSession? = null
+    private var player: ExoPlayer? = null
+
     override fun onCreate() {
         super.onCreate()
-        val player = ExoPlayer.Builder(this).build()
-        mediaSession = MediaSession.Builder(this, player).build()
+        player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player!!).setId("MyMediaSession").build()
     }
 
-    override fun onGetSession(
-        controllerInfo: MediaSession.ControllerInfo
-    ): MediaSession? = mediaSession
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return mediaSession
+    }
 
     override fun onDestroy() {
-        mediaSession?.run {
-            player.release()
-            release()
-            mediaSession = null
-        }
+        mediaSession?.release()
+        player?.release()
+        mediaSession = null
+        player = null
         super.onDestroy()
     }
 }
