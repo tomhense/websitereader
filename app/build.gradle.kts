16a176972754bc9749a8b66c891f9e372046469f
Tom <example.com>
2026-06-06 14:54:06 +0200

    SDK upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.websitereader"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
